e991ad7bd452b68d2fcf4f019301a3f4c192c061
Yu Shi <yushi@mac-mini.mynetworksettings.com>
2022-10-28 20:19:39 -0400

    working on the header

---
+++ src/components/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBox = () => {
+  return (
+    <div className="search">
+        <input type="text" placeholder="Find Your Anime..." />
+    </div>
+  )
+}
+
+export default SearchBox
---
+++ src/data/data.js
@@ -0,0 +1,19 @@
+// navigation
+export const navigationItems = [
+    {
+      name: "Home",
+      href: "home",
+    },
+    {
+      name: "Movies",
+      href: "movies",
+    },
+    {
+      name: "Most Popular",
+      href: "most popular",
+    },
+    {
+      name: "Top Airing",
+      href: "top airing",
+    },
+  ];
