9a9ed59fef43e59238986bf912ea903d8e2ec5e3
Yu Shi <yushi@mac-mini.mynetworksettings.com>
2022-11-02 22:34:16 -0400

    working on anime details

---
+++ src/components/AnimeGenres.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AnimeGenres = ({ genre }) => {
+  return <span>{genre}</span>;
+};
+
+export default AnimeGenres;
--- src/components/pages/AnimeDetails.js
+++ src/components/pages/AnimeDetails.js
@@ -1,25 +1,25 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchAnimeDetails } from "../../data/fetchAnimeApi";
+import AnimeDetailsCard from "../AnimeDetailsCard";
 
 const AnimeDetails = () => {
   const { id } = useParams();
   const [animeDetails, setAnimeDetails] = useState(null);
 
-  // const animeTitle = id.replace(id.substring(id.indexOf('-episode') + 1),"").slice(0, -1);
-
   useEffect(() => {
     fetchAnimeDetails(`${id}`)
       .then((data) => data)
       .then((data) => {
-        console.log(data)
         setAnimeDetails(data);
       });
 
   }, [id]);
 
   return (
-      <div>AnimeDetails</div>
+      <div className="container details-body">
+        <AnimeDetailsCard animeDetails={animeDetails} />
+      </div>
   ) 
 };
 
