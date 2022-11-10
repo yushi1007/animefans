482ee311f50c74ed65b7485c49083eb11cd4a7f7
Yu Shi <yushi@macbook-pro.mynetworksettings.com>
2022-11-09 23:10:12 -0500

    working on the home section slider

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -7,30 +7,7 @@ const HomeCard = ({
 }) => {
   return (
     <div className="box">
-      <div className="coverImage">
-        <img src={animeImg} alt="anime_image" />
-      </div>
-      <div className="content flex">
-        <div className="details row">
-          <h1>{animeTitle}</h1>
-          <div className="cast">
-            <h4>
-              <span>Latest Episode</span>
-              {latestEp}
-            </h4>
-            <h4>
-              <span>Genres</span>
-              {genres?.map((genre, index) => {
-                return <h4 key={index}>{genre}</h4>;
-              })}
-            </h4>
-          </div>
-          <button className="watchnow-btn">
-            <FontAwesomeIcon className="icon" icon={faPlay} />
-            <span>Watch Now</span>
-          </button>
-        </div>
-      </div>
+     
     </div>
   );
 };
