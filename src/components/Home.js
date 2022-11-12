2e5b0766a22a4abd39a5e0e8eb6e2b10b5cefaea
Yu Shi <yushi@Yus-MacBook-Pro.local>
2022-11-12 11:38:23 -0500

    worked on the home section anime

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import "swiper/css";
 import "../styles/styles.css";
 import "swiper/css/pagination";
-import { Lazy, Pagination, Navigation } from "swiper";
+import { Autoplay, Pagination, Navigation } from "swiper";
 import "swiper/css/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 const Home = ({ topAiring }) => {
+  const [swiperRef, setSwiperRef] = useState(null);
+
+  const prevHandler = () => {
+    swiperRef.slidePrev();
+  };
+
+  const nextHandler = () => {
+    swiperRef.slideNext();
+  };
+
   return (
     <div id="main-slider">
       <Swiper
-      style={{
-        "--swiper-navigation-color": "#fff",
-        "--swiper-pagination-color": "#fff",
-      }}
+        onSwiper={(swiper) => setSwiperRef(swiper)}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
         slidesPerView={1}
         spaceBetween={10}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
         breakpoints={{
           640: {
             slidesPerView: 2,
             spaceBetween: 20,
           },
           768: {
-            slidesPerView: 4,
+            slidesPerView: 2,
             spaceBetween: 40,
           },
           1024: {
@@ -35,8 +42,8 @@ const Home = ({ topAiring }) => {
             spaceBetween: 50,
           },
         }}
-        modules={[Lazy, Navigation]}
         className="mySwiper"
+        modules={[Autoplay]}
       >
         {topAiring.map((topAiringItem, index) => {
           return (
@@ -45,6 +52,10 @@ const Home = ({ topAiring }) => {
             </SwiperSlide>
           );
         })}
+        <div className="slider-btns">
+          <div className="swiper-button-prev" onClick={prevHandler}></div>
+          <div className="swiper-button-next" onClick={nextHandler}></div>
+        </div>
       </Swiper>
     </div>
   );
