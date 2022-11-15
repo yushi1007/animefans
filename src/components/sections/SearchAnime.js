d84457ba219d97e831d197358967586ef9860ea6
Yu Shi <yushi@mac-mini.mynetworksettings.com>
2022-11-14 22:23:40 -0500

    worked on the loader for search anime

--- src/components/sections/SearchAnime.js
+++ src/components/sections/SearchAnime.js
@@ -3,22 +3,29 @@ import { useParams, useLocation } from "react-router-dom";
 import { fetchSearchTerm } from "../../data/fetchAnimeApi";
 import AnimeLists from "../AnimeLists";
 import ErrorMessage from "../ErrorMessage";
+import Loader from "../Loader";
 
 const SearchAnime = () => {
   const location = useLocation();
   const { searchTerm } = useParams();
   const [searchedAnime, setSearchedAnime] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
+    setSearchedAnime([]);
     const getSearchTerm = (term) => {
       fetchSearchTerm(`?keyw=${term}`).then((data) => {
-        console.log(data);
         setSearchedAnime(data);
+        if (searchedAnime) {
+          setIsLoading(false);
+        }
       });
     };
     getSearchTerm(searchTerm);
   }, [searchTerm]);
 
+  if(isLoading) return <Loader />;
   return (
     <React.Fragment>
       {searchedAnime.length === 0 ? (
