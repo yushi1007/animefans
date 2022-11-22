f25ad4a4218df6925b01f6c9d9051322cf6204f8
Yu Shi <yushi@mac-mini.mynetworksettings.com>
2022-11-22 17:17:46 -0500

    add condition for episode list and change some style

--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import EpisodeCard from "./EpisodeCard";
 
-const EpisodeList = ({ animeDetails }) => {
+const EpisodeList = ({ animeDetails, handleStatus }) => {
   const sortEpisodes = animeDetails?.episodesList?.sort(
     (a, b) => a.episodeNum - b.episodeNum
   );
@@ -11,10 +11,14 @@ const EpisodeList = ({ animeDetails }) => {
   });
 
   return (
-    <div className="lists-box ep-box">
-      <h1>Episodes</h1>
-      <div className="episodes-box">{episodeItems}</div>
-    </div>
+    <React.Fragment>
+      {!handleStatus ? (
+        <div className="lists-box ep-box">
+          <h1>Episodes</h1>
+          <div className="episodes-box">{episodeItems}</div>
+        </div>
+      ) : null}
+    </React.Fragment>
   );
 };
 
