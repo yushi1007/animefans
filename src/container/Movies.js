49f7e884f04443a41ff11a32a54525c928fcff71
Yu Shi <yushi@yus-mac-mini.mynetworksettings.com>
2022-12-30 23:32:07 -0500

    worked on the paginator

--- src/container/Movies.js
+++ src/container/Movies.js
@@ -10,37 +10,7 @@ const Movies = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [pageNumberLimit, setPageNumberLimit] = useState(5);
-  const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5);
-  const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
-
-  const handlePageClick = (event) => {
-    setCurrentPage(Number(event.target.id));
-  };
-
-  const pages = [];
-  for (let i = 1; i <= 89; i++) {
-    pages.push(i);
-  }
-
-  const renderPageNumbers = pages.map((pageNumber) => {
-    if (
-      pageNumber < maxPageNumberLimit + 1 &&
-      pageNumber > minPageNumberLimit
-    ) {
-      return (
-        <Paginator
-          key={pageNumber}
-          id={pageNumber}
-          pageNumber={pageNumber}
-          currentPage={currentPage}
-          handlePageClick={handlePageClick}
-        />
-      );
-    } else {
-      return null;
-    }
-  });
+  const totalPages = 89;
 
   useEffect(() => {
     setIsLoading(true);
@@ -52,32 +22,6 @@ const Movies = () => {
       }
     });
   }, [currentPage]);
-
-  const handleNextButton = () => {
-    setCurrentPage(currentPage + 1);
-    if (currentPage + 1 > maxPageNumberLimit) {
-      setMaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
-      setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit);
-    }
-  };
-
-  const handlePrevButton = () => {
-    setCurrentPage(currentPage - 1);
-    if ((currentPage - 1) % pageNumberLimit === 0) {
-      setMaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
-      setMinPageNumberLimit(minPageNumberLimit - pageNumberLimit);
-    }
-  };
-
-  let pageIncrementButton = null;
-  if (pages.length > maxPageNumberLimit) {
-    pageIncrementButton = <li onClick={handleNextButton}> &hellip; </li>;
-  }
-
-  let pageDecrementButton = null;
-  if (minPageNumberLimit >= 1) {
-    pageDecrementButton = <li onClick={handlePrevButton}> &hellip; </li>;
-  }
 
   return (
     <>
@@ -95,29 +39,11 @@ const Movies = () => {
             <h1>Movies</h1>
             <AnimeLists animeLists={movies} />
           </div>
-          <div className="paginator">
-            <ul className="page-numbers">
-              <li
-                onClick={handlePrevButton}
-                disabled={currentPage === pages[0] ? true : false}
-              >
-                Prev
-              </li>
-              {pageDecrementButton}
-              {renderPageNumbers}
-              {pageIncrementButton}
-              <li
-                onClick={handleNextButton}
-                disabled={
-                  currentPage === pages[pages.length - 1] ? true : false
-                }
-              >
-                Next
-              </li>
-            </ul>
-          </div>
         </div>
       )}
+      <div className="paginator">
+        <Paginator totalPages={totalPages} setCurrentPage={setCurrentPage} />
+      </div>
     </>
   );
 };
--- src/container/Popular.js
+++ src/container/Popular.js
@@ -11,37 +11,7 @@ const Popular = () => {
   const [popular, setPopular] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [pageNumberLimit, setPageNumberLimit] = useState(5);
-  const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5);
-  const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
-
-  const handlePageClick = (event) => {
-    setCurrentPage(Number(event.target.id));
-  };
-
-  const pages = [];
-  for (let i = 1; i <= 504; i++) {
-    pages.push(i);
-  }
-
-  const renderPageNumbers = pages.map((pageNumber) => {
-    if (
-      pageNumber < maxPageNumberLimit + 1 &&
-      pageNumber > minPageNumberLimit
-    ) {
-      return (
-        <Paginator
-          key={pageNumber}
-          id={pageNumber}
-          pageNumber={pageNumber}
-          currentPage={currentPage}
-          handlePageClick={handlePageClick}
-        />
-      );
-    } else {
-      return null;
-    }
-  });
+  const totalPages = 504;
 
   useEffect(() => {
     setIsLoading(true);
@@ -53,32 +23,6 @@ const Popular = () => {
       }
     });
   }, [currentPage]);
-
-  const handleNextButton = () => {
-    setCurrentPage(currentPage + 1);
-    if (currentPage + 1 > maxPageNumberLimit) {
-      setMaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
-      setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit);
-    }
-  };
-
-  const handlePrevButton = () => {
-    setCurrentPage(currentPage - 1);
-    if ((currentPage - 1) % pageNumberLimit === 0) {
-      setMaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
-      setMinPageNumberLimit(minPageNumberLimit - pageNumberLimit);
-    }
-  };
-
-  let pageIncrementButton = null;
-  if (pages.length > maxPageNumberLimit) {
-    pageIncrementButton = <li onClick={handleNextButton}> &hellip; </li>;
-  }
-
-  let pageDecrementButton = null;
-  if (minPageNumberLimit >= 1) {
-    pageDecrementButton = <li onClick={handlePrevButton}> &hellip; </li>;
-  }
 
   return (
     <>
@@ -96,29 +40,11 @@ const Popular = () => {
             <h1>Popular</h1>
             <AnimeLists animeLists={popular} />
           </div>
-          {location.pathname === "/popular" && (
-            <div className="paginator">
-              <ul className="page-numbers">
-                <li
-                  onClick={handlePrevButton}
-                  disabled={currentPage === pages[0] ? true : false}
-                >
-                  Prev
-                </li>
-                {pageDecrementButton}
-                {renderPageNumbers}
-                {pageIncrementButton}
-                <li
-                  onClick={handleNextButton}
-                  disabled={
-                    currentPage === pages[pages.length - 1] ? true : false
-                  }
-                >
-                  Next
-                </li>
-              </ul>
-            </div>
-          )}
+        </div>
+      )}
+      {location.pathname === "/popular" && (
+        <div className="paginator">
+          <Paginator totalPages={totalPages} setCurrentPage={setCurrentPage} />
         </div>
       )}
     </>
