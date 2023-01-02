29956ad646806fb1a46f7975c9243eb2bfd9f257
Yu Shi <yushi@yus-mac-mini.mynetworksettings.com>
2023-01-02 17:32:11 -0500

    update the footer

--- src/container/TopAiring.js
+++ src/container/TopAiring.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { fetchTopAiring } from "../data/fetchAnimeApi";
 import AnimeLists from "../components/AnimeLists";
-import Loader from "../components/Loader";
 
 const TopAiring = () => {
   const location = useLocation();
